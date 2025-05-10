67f3d109183ae6608cbd3257ce007613139f909f
sloss <145279051+s1oss@users.noreply.github.com>
2025-05-10 21:44:11 +0200

    feat(data entry): added region predictions

--- region_dict.py
+++ region_dict.py
@@ -7,19 +7,11 @@ region_dict = {
 "Breisach": ["Breisach am Rhein, Stadt"]
 }
 
+# in MW. 
 forcast_dict = {
-    "Breisach": ["Breisach am Rhein, Stadt"],
-    "Freiburg": ["Freiburg im Breisgau, Stadt", "Umkirch"],
-    "Kaiserstuhl": ["Ihringen", "Weisweil"],
-    "RWH": ["Lauchringen", "Wutöschingen"],
-    "Süd": ["Lauchingen", "Neuenburg am Rhein, Stadt", "Bad Krozingen, Stadt",
-            "Ebringen", "Pfaffenweiler", "Heitersheim, Stadt"]
+        "Breisach": {"2023": 19.42, "2028": 83.77, "2033": 137.4, "2045": 241.72},
+    "Freiburg": {"2023": 68.41, "2028": 177.48, "2033": 268.38, "2045": 408.9},
+    "Kaiserstuhl": {"2023": 4.59, "2028": 21.92, "2033": 36.37, "2045": 58.42},
+    "RWH": {"2023": 11.14, "2028": 33.7, "2033": 52.51, "2045": 70.9},
+    "Süd": {"2023": 49.89, "2028": 226.41, "2033": 373.51, "2045": 676.72}
 }
-
-# // ['Stegen' '' '' 'Merzhausen' 'Merdingen'
-# //  '' 'Gottenheim' ''
-# //  'Hartheim am Rhein' 'Buggingen' ''
-# //  'Müllheim, Stadt' 'Au' 'Bad Krozingen, Stadt' '' 'Eschbach'
-# //  'Heitersheim, Stadt' 'Weisweil' 'Ebringen' ''
-# //  'Staufen im Breisgau, Stadt' 'Horben' 'Oberried' 'Buchenbach' 'St. Peter'
-# //  'Titisee-Neustadt, Stadt' '']
