0174d8d957a1e5821712b0e731ddd3de1a5a270e
Martin Peters <martin.peters@students.uni-freiburg.de>
2025-05-10 21:24:56 +0200

    dict with the badenova regions

---
+++ region_dict.py
@@ -0,0 +1,25 @@
+region_dict = {
+"Süd": ["Lauchingen", "Neuenburg am Rhein, Stadt", "Bad Krozingen, Stadt",
+"Ebringen", "Pfaffenweiler", "Heitersheim, Stadt"],
+"Kaiserstuhl": ["Ihringen", "Weisweil"],
+"RWH": ["Lauchringen", "Wutöschingen"],
+"Freiburg": ["Freiburg im Breisgau, Stadt", "Umkirch"],
+"Breisach": ["Breisach am Rhein, Stadt"]
+}
+
+forcast_dict = {
+    "Breisach": ["Breisach am Rhein, Stadt"],
+    "Freiburg": ["Freiburg im Breisgau, Stadt", "Umkirch"],
+    "Kaiserstuhl": ["Ihringen", "Weisweil"],
+    "RWH": ["Lauchringen", "Wutöschingen"],
+    "Süd": ["Lauchingen", "Neuenburg am Rhein, Stadt", "Bad Krozingen, Stadt",
+            "Ebringen", "Pfaffenweiler", "Heitersheim, Stadt"]
+}
+
+# // ['Stegen' '' '' 'Merzhausen' 'Merdingen'
+# //  '' 'Gottenheim' ''
+# //  'Hartheim am Rhein' 'Buggingen' ''
+# //  'Müllheim, Stadt' 'Au' 'Bad Krozingen, Stadt' '' 'Eschbach'
+# //  'Heitersheim, Stadt' 'Weisweil' 'Ebringen' ''
+# //  'Staufen im Breisgau, Stadt' 'Horben' 'Oberried' 'Buchenbach' 'St. Peter'
+# //  'Titisee-Neustadt, Stadt' '']
